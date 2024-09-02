f6ae116d158fc9d5a50311f4590e717bd82f892f
Shane Huntley <huntsman@gmail.com>
2024-09-02 15:40:41 +1000

    Task 1.3 Chain Rule

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -162,9 +162,8 @@ class Scalar:
         assert h is not None
         assert h.last_fn is not None
         assert h.ctx is not None
-
-        # TODO: Implement for Task 1.3.
-        raise NotImplementedError('Need to implement for Task 1.3')
+        back_vals = h.last_fn._backward(h.ctx, d_output)
+        return tuple(zip(h.inputs, back_vals))
 
     def backward(self, d_output: Optional[float] = None) -> None:
         """
